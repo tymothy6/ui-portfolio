b18782d2296195b6778616562e95b9ec67db256e
Tim Ng <108041576+tymothy6@users.noreply.github.com>
2023-10-30 22:31:31 -0400

    card hover changes

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -20,14 +20,20 @@ interface ProjectCardProps {
 
 export const ProjectCard = React.forwardRef<HTMLDivElement, ProjectCardProps>(({ data, isFirstChild, noHover }, ref) => {
     const [isClient, setIsClient] = React.useState(false)
+    const [isHovered, setIsHovered] = React.useState(false)
 
     React.useEffect(() => {
         setIsClient(true)
     }, [])
 
     return(
+        <Card 
+        ref={ref} 
+        tabIndex={0} 
+        className={`${isFirstChild ? 'mx-8' : 'ml-4 mr-8'} md:mx-0 focus-visible:outline-none focus-visible:ring focus-visible:ring-primary focus-visible:ring-offset-2 focus-visible:ring-offset-background ${noHover ? '' : 'hover:animate-card-translate-y'}`}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}>
         <Link tabIndex={-1} href={`/work/${data.slug}`}>
-        <Card ref={ref} tabIndex={0} className={`${isFirstChild ? 'mx-8' : 'ml-4 mr-8'} md:mx-0 focus-visible:outline-none focus-visible:ring focus-visible:ring-primary focus-visible:ring-offset-2 focus-visible:ring-offset-background ${noHover ? '' : 'hover:border-2 hover:border-primary hover:animate-card-translate-y'}`}>
             {isClient && (
                 <>
             <CardImageHeader>
@@ -38,7 +44,7 @@ export const ProjectCard = React.forwardRef<HTMLDivElement, ProjectCardProps>(({
             </CardImageHeader>
             <CardContent className="flex flex-row justify-between items-start pt-4 h-full">
                 <div className="flex flex-col">
-                    <p className="text-left text-lg font-semibold text-foreground pr-2 mb-0">{data.name}</p>
+                    <p className={`text-left text-lg font-semibold text-foreground pr-2 mb-0 ${isHovered ? 'underline decoration-primary decoration-4 underline-offset-2' : ''}`}>{data.name}</p>
                     <p className="text-left text-base md:text-lg font-semibold text-gray-800 dark:text-gray-200 pr-2 mb-2">{data.type}</p> 
                     <div className="flex flex-row flex-wrap gap-2">
                         <Badge variant="default"><span className="font-mono font-semibold">{data.year}</span></Badge>
@@ -56,9 +62,10 @@ export const ProjectCard = React.forwardRef<HTMLDivElement, ProjectCardProps>(({
             </CardContent>
             </>
             )}  
+        </Link>
         </Card>
        
-        </Link>
+        
         
     )
 }
