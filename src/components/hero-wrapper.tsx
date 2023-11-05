edcbaa93880904378c16e9ede9d372f661df0070
Tim Ng <108041576+tymothy6@users.noreply.github.com>
2023-11-05 18:48:57 -0500

    revised margin fix

--- src/components/hero-wrapper.tsx
+++ src/components/hero-wrapper.tsx
@@ -22,7 +22,7 @@ export function HeroWrapper ( { id }: { id: string }) {
           },
         }}>
         <div 
-        className="relative flex-grow flex items-center justify-center min-h-[100vh] mt-32 md:mt-0">
+        className="relative flex grow items-center justify-center min-h-[100vh] py-32">
             <GridPattern />
             <Hero id={id} />
         </div>
