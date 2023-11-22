ad2767b6f694d96d29805ec0c4ae384dc84c1235
Tim Ng <108041576+tymothy6@users.noreply.github.com>
2023-11-21 20:50:11 -0500

    blog grid flex fix

--- src/components/blog-grid.tsx
+++ src/components/blog-grid.tsx
@@ -14,7 +14,7 @@ export async function BlogPostGrid () {
                     <p className="text-base md:text-lg font-semibold text-foreground font-mono">Posts</p>
                     <p className="text-base md:text-lg font-medium text-gray-700 dark:text-gray-400 font-mono hidden md:block">· 2023 - </p>
                 </div>
-                <div className="flex-row gap-4 z-[5] mr-8 md:mr-0">
+                <div className="flex flex-row gap-2 mr-8 md:mr-0">
                     <div className="hidden md:block"><SortButton data={posts} /></div>
                     <BlogCombobox data={posts} />
                 </div>
