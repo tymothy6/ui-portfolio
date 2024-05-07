8592b8da6e8dab4e7a3b01a51053fbce45dec89b
Tim Ng <108041576+tymothy6@users.noreply.github.com>
2024-05-07 16:02:19 -0400

    update bmc

--- src/components/patterns/buy-me-coffee.tsx
+++ src/components/patterns/buy-me-coffee.tsx
@@ -1,17 +1,15 @@
-import { ArrowTopRightIcon } from "@radix-ui/react-icons";
 
 export function BuyMeCoffee () {
     return (
         <div className="flex items-center gap-2 mx-8 md:mx-0">
-            <h4 className="text-foreground font-medium text-sm">Liked my writing?</h4>
+            <h4 className="text-foreground font-medium text-base">Liked my writing?</h4>
             <a 
             href="https://buymeacoffee.com/timng"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-muted-foreground hover:text-foreground hover:underline font-regular text-sm"
+            className="text-muted-foreground hover:text-foreground hover:underline font-medium text-base"
             >
                 Buy me a coffee! ☕️
-                <ArrowTopRightIcon className="h-4 w-4" />
             </a>
         </div>
     )
