8d1a3bd35b4a1984dd1f2bba8b469f3b8c9100f7
Tim Ng <108041576+tymothy6@users.noreply.github.com>
2024-07-21 22:54:25 -0700

    Add invalidate to three fiber

--- src/components/canvas/grid.tsx
+++ src/components/canvas/grid.tsx
@@ -2,7 +2,7 @@
 
 import { useTheme } from "next-themes"
 import React, { useRef, useState, useEffect } from "react"
-import { Canvas, useFrame, extend } from "@react-three/fiber"
+import { Canvas, useFrame, extend, invalidate } from "@react-three/fiber"
 import { Mesh, Vector3 } from "three"
 import { Line } from "@react-three/drei"
 import { PlaneGeometry, BufferGeometry, BufferAttribute, LineSegments, MeshBasicMaterial } from "three"
@@ -20,7 +20,6 @@ type SquareProps = {
     activeAnimation: {i: number, j: number} | null;
     setActiveAnimation: React.Dispatch<React.SetStateAction<{i: number, j: number} | null>>;
   };
-
 
 const Square: React.FC<SquareProps> = ({ position, opacity = 0.1, cellSize = 0.5, changeFrequency = 0.99, maxOpacity, activeAnimation, setActiveAnimation, i, j }) => {
     const meshRef = useRef<Mesh>() as React.MutableRefObject<Mesh>;
@@ -51,6 +50,7 @@ const Square: React.FC<SquareProps> = ({ position, opacity = 0.1, cellSize = 0.5
             const material = meshRef.current.material as MeshBasicMaterial;
             material.opacity = 1.0; // set opacity on hover
             material.needsUpdate = true;
+            invalidate(); // manually trigger a render
         }
     };
 
@@ -60,15 +60,16 @@ const Square: React.FC<SquareProps> = ({ position, opacity = 0.1, cellSize = 0.5
             const material = meshRef.current.material as MeshBasicMaterial;
             material.opacity = opacity; // reset opacity on hover out
             material.needsUpdate = true;
+            invalidate(); //manually trigger a render
         }
     };
   
     return (
       <mesh
-      ref={meshRef}
-      position={position}
-      onPointerOver={handlePointerOver}
-      onPointerOut={handlePointerOut}
+        ref={meshRef}
+        position={position}
+        onPointerOver={handlePointerOver}
+        onPointerOut={handlePointerOut}
       >
         <planeGeometry attach="geometry" args={[cellSize, cellSize]} />
         <meshBasicMaterial attach="material" color={gridColor} transparent />
@@ -77,7 +78,6 @@ const Square: React.FC<SquareProps> = ({ position, opacity = 0.1, cellSize = 0.5
   };
 
   const GridPattern = () => {
-    
     const [gridSize, setGridSize] = useState(48);
     const [activeAnimation, setActiveAnimation] = useState<{i: number, j: number} | null>(null);
     const spacing = 0.5;
@@ -97,56 +97,56 @@ const Square: React.FC<SquareProps> = ({ position, opacity = 0.1, cellSize = 0.5
             } else {
                 setGridSize(24);
             }
+            invalidate(); // manually trigger a render after resizing the screen and grid size is adjusted
         }
         handleResize();
 
         window.addEventListener("resize", handleResize);
         return () => window.removeEventListener("resize", handleResize);
-    }, [])
+    }, []);
 
     return (
-    <div className={`w-full h-auto absolute inset-0 ${isDarkTheme ? 'opacity-100' : 'opacity-50'}`}>
-        <div style={{ background: `
-        linear-gradient(to bottom, transparent, transparent 60%, ${isDarkTheme ? 'rgba(15, 23, 42, 0.95)' : 'rgba(255, 255, 255, 1.0)'} 100%),
-        radial-gradient(circle at center, transparent, ${isDarkTheme ? 'rgba(15, 23, 42, 0.9)' : 'rgba(255, 255, 255, 1.0)'} 100%)
-        ` }} className="absolute top-0 left-0 w-full h-full z-10 pointer-events-none backdrop-blur-[0.4px]"></div>
-      <Canvas className="absolute top-0 left-0 w-full h-full z-0">
-        {[...Array(gridSize)].map((_, i) =>
-          [...Array(gridSize)].map((_, j) => (
-            <Square 
-            key={`${i}-${j}`} 
-            position={[(i - gridSize / 2) * spacing, (j - gridSize / 2) * spacing, 0]} i={i} j={j} 
-            gridSize={gridSize}
-            activeAnimation={activeAnimation}
-            setActiveAnimation={setActiveAnimation}
-            />
-          ))
-        )}
-        {[...Array(gridSize + 1)].map((_, i) => (
-            <Line 
-                key={`vline-${i}`} 
-                points={[
-                    new Vector3((i - gridSize / 2) * spacing - halfSpacing, -gridHalfSize, 0),
-                    new Vector3((i - gridSize / 2) * spacing - halfSpacing, gridHalfSize, 0)
-                ]}
-                color={lineColor}
-                lineWidth={0.5}
-            />
-        ))}
-        {[...Array(gridSize + 1)].map((_, i) => (
-            <Line 
-                key={`hline-${i}`} 
-                points={[
-                    new Vector3(-gridHalfSize, (i - gridSize / 2) * spacing - halfSpacing, 0),
-                    new Vector3(gridHalfSize, (i - gridSize / 2) * spacing - halfSpacing, 0)
-                ]}
-                color={lineColor}
-                lineWidth={0.5}
-            />
-        ))}
-        
-      </Canvas>
-      </div>
+        <div className={`w-full h-auto absolute inset-0 ${isDarkTheme ? 'opacity-100' : 'opacity-50'}`}>
+            <div style={{ background: `
+            linear-gradient(to bottom, transparent, transparent 60%, ${isDarkTheme ? 'rgba(15, 23, 42, 0.95)' : 'rgba(255, 255, 255, 1.0)'} 100%),
+            radial-gradient(circle at center, transparent, ${isDarkTheme ? 'rgba(15, 23, 42, 0.9)' : 'rgba(255, 255, 255, 1.0)'} 100%)
+            ` }} className="absolute top-0 left-0 w-full h-full z-10 pointer-events-none backdrop-blur-[0.4px]" />
+            <Canvas frameloop="demand" className="absolute top-0 left-0 w-full h-full z-0">
+                {[...Array(gridSize)].map((_, i) =>
+                [...Array(gridSize)].map((_, j) => (
+                    <Square 
+                    key={`${i}-${j}`} 
+                    position={[(i - gridSize / 2) * spacing, (j - gridSize / 2) * spacing, 0]} i={i} j={j} 
+                    gridSize={gridSize}
+                    activeAnimation={activeAnimation}
+                    setActiveAnimation={setActiveAnimation}
+                    />
+                ))
+                )}
+                {[...Array(gridSize + 1)].map((_, i) => (
+                    <Line 
+                        key={`vline-${i}`} 
+                        points={[
+                            new Vector3((i - gridSize / 2) * spacing - halfSpacing, -gridHalfSize, 0),
+                            new Vector3((i - gridSize / 2) * spacing - halfSpacing, gridHalfSize, 0)
+                        ]}
+                        color={lineColor}
+                        lineWidth={0.5}
+                    />
+                ))}
+                {[...Array(gridSize + 1)].map((_, i) => (
+                    <Line 
+                        key={`hline-${i}`} 
+                        points={[
+                            new Vector3(-gridHalfSize, (i - gridSize / 2) * spacing - halfSpacing, 0),
+                            new Vector3(gridHalfSize, (i - gridSize / 2) * spacing - halfSpacing, 0)
+                        ]}
+                        color={lineColor}
+                        lineWidth={0.5}
+                    />
+                ))} 
+            </Canvas>
+        </div>
     );
   };
 
