49c26a903c19d6b8e33d85c885265b84b0af2f3c
Tim Ng <108041576+tymothy6@users.noreply.github.com>
2025-08-07 11:04:53 -0700

    fix blog skeleton container width

--- src/components/patterns/grid-skeleton.tsx
+++ src/components/patterns/grid-skeleton.tsx
@@ -2,7 +2,7 @@ import { BlogCardSkeleton } from "@/components/patterns/card-skeleton";
 
 export function BlogGridSkeleton() {
   return (
-    <div className="flex flex-row md:grid md:grid-cols-2 xl:grid-cols-3 justify-center items-center gap-4 overflow-x-hidden">
+    <div className="flex flex-row md:grid md:grid-cols-2 xl:grid-cols-3 justify-center items-center gap-4 overflow-x-hidden w-full">
       <BlogCardSkeleton />
       <BlogCardSkeleton />
       <BlogCardSkeleton />
